3a6a89b47291e5e931f22a20a5e2229f9fecfb38
Ivan Camilo Buitrago <73039901+codigo-natural@users.noreply.github.com>
2024-04-29 08:57:55 -0500

    feat 🎉 add Route Home page, component Navbar and global styles (#8)
    * feat 🎉 cahnge tailwindcss for styles with sass and add folder structure for routes
    
    * resolve conflicts
    
    * feat 🎉 add Route Home page, component Navbar and global styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { HomePage } from './routes/homePage'
+import { Layout } from './routes/layout'
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">InmoChat</h1>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        }
+      ]
+    }
+  ])
+  return <RouterProvider router={router} />
 }
 
 export default App
