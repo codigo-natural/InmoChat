ed05ae28da27dc784f3b53e66529ffe7079fc281
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-05 18:38:03 -0500

    implement ⚡ expresjs prisma crud in update  info user profile

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8080, () => {
   console.log("Server started on port 8080");
