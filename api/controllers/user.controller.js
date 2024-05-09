2bad3dd3d37fcb833ce15760a33868ee45dcbf9c
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-09 14:10:56 -0500

    feat ✨  implement endpoint to save posts and create schema to save posts

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -74,3 +74,36 @@ export const deleteUser = async (req, res) => {
     res.status(500).json({ message: "Failed to delete user!" });
   }
 };
+
+export const savePost = async (req, res) => {
+  const postId = req.body.postId;
+  const tokenUserId = req.userId;
+
+  try {
+    const savedPost = await prisma.savedPost.findUnique({
+      where: {
+        userId_postId: {
+          userId: tokenUserId,
+          postId,
+        },
+      },
+    });
+
+    if (savedPost) {
+      await prisma.savedPost.delete({
+        where: {
+          id: savedPost.id,
+        },
+      });
+      res.status(200).json({ message: "Post removed from saved list" });
+    } else {
+      await prisma.savedPost.create({
+        data: {
+          userId: tokenUserId,
+          postId,
+        },
+      });
+      res.status(200).json({ message: "post saved" });
+    }
+  } catch (error) {}
+};
