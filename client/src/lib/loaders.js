afb9cb2352aa42fc04081fc840394c2be653955f
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-09 11:51:30 -0500

    feat ✨  React fetch data based on url query

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -4,3 +4,9 @@ export const singlePageLoader = async ({ request, params }) => {
   const res = await apiRequest("/posts/" + params.id);
   return res.data;
 };
+
+export const listPageLoader = async ({ request, params }) => {
+  const query = request.url.split('?')[1]
+  const postResponse = await apiRequest("/posts?" + query);
+  return postResponse.data;
+};
