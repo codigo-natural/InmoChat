cf734896ad13099190c5fd3ebb10401ca4e14026
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-12 17:09:20 -0500

    adding a dependency ➕ socket context

---
+++ client/src/context/SocketContext.jsx
@@ -0,0 +1,26 @@
+// client/src/context/socketcontext.jsx
+import { createContext, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client"
+import { AuthContext } from "./AuthContext";
+
+export const SocketContext = createContext()
+
+/* eslint-disable react/prop-types */
+export const SocketContextProvider = ({ children }) => {
+  const { currentUser } = useContext(AuthContext)
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    setSocket(io("http://localhost:4000"))
+  }, [])
+
+  useEffect(() => {
+    currentUser && socket?.emit("newUser", currentUser.id)
+  }, [currentUser, socket])
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  )
+}
